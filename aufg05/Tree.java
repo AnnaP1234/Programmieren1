9ec8ab3589783046a1af0b430d2deeab545e6dbf
unknown <sophie.ludewig@tu-braunschweig.de>
2019-01-07 15:58:21 +0100

    Stand Montag 15:58

--- aufg05/Tree.java
+++ aufg05/Tree.java
@@ -10,9 +10,42 @@ public class Tree {
 
     // region ctor
 
+    public Tree(int pValue) {
+        value = pValue;
+    }
+
     // endregion ctor
 
     // region methods
+
+    /**
+     * Gibt den Wert der Wurzel wieder
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * Gibt den linken Teilbaum wieder
+     */
+    public Tree getLeftChild() {
+        return leftChild;
+    }
+
+    /**
+     * Gibt den rechten Teilbaum wieder
+     */
+    public Tree getRightTree() {
+        return rightChild;
+    }
+
+    /**
+     * Fügt einen neuen Wert in den aktuellen Baum ein
+     * @param newValue Der einzufügende Wert
+     */
+    public void insert(int newValue) {
+        
+    }
 
     // endregion methods
 }
