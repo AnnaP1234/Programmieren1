e1f2f5c4a4a1ce668dadc8ca7378b49492f0071f
unknown <sophie.ludewig@tu-braunschweig.de>
2019-01-07 15:17:24 +0100

    Beginn Arbeit Aufg05

---
+++ aufg05/Tree.java
@@ -0,0 +1,18 @@
+public class Tree {
+
+    // region vars
+
+    private int value;
+    private Tree leftChild;
+    private Tree rightChild;
+
+    // endregion vars
+
+    // region ctor
+
+    // endregion ctor
+
+    // region methods
+
+    // endregion methods
+}
